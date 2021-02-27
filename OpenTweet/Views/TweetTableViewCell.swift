217826f72d231407e7c9e7bb4c314b71573c7eee
Steve Schwedt <sschwedt@gmail.com>
2021-02-27 10:12:07 -0700

    Adds formatting to highlight users mentioned in tweets

--- OpenTweet/Views/TweetTableViewCell.swift
+++ OpenTweet/Views/TweetTableViewCell.swift
@@ -19,7 +19,7 @@ class TweetTableViewCell: UITableViewCell {
 
     func configure(tweet: Tweet) {
         authorLabel.text = tweet.author
-        contentLabel.text = tweet.content
+        contentLabel.attributedText = formattedContent(from: tweet.content)
         dateLabel.text = DateFormatter.yearMonthDayTimeString(from: tweet.date)
         
         avatarImage.downloadImage(urlString: tweet.avatar, fallbackSymbol: "person.circle.fill")
@@ -29,4 +29,21 @@ class TweetTableViewCell: UITableViewCell {
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    
+    private func formattedContent(from content: String) -> NSAttributedString {
+        let words = content.split(separator: " ")
+        let formattedContent = NSMutableAttributedString()
+        
+        for word in words {
+            var subString: NSAttributedString
+            if word.starts(with: "@") {
+                subString = NSAttributedString(string: String(word) + " ", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15), NSAttributedString.Key.foregroundColor: UIColor.systemBlue])
+            } else {
+                subString = NSAttributedString(string: String(word) + " ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)])
+            }
+            formattedContent.append(subString)
+        }
+        
+        return formattedContent
+    }
 }
